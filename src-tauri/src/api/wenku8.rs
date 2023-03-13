23b8d2b58188fdb1c51c1622c60d6dc35640524c
yixinBC <yixinbc@foxmail.com>
2023-03-13 22:10:35 +0800

    chore:be:wenku8 login

--- src-tauri/src/api/wenku8.rs
+++ src-tauri/src/api/wenku8.rs
@@ -1,1 +1,29 @@
 use crate::utils::fake_ua;
+
+async fn login(cookie_jar: reqwest::cookie::Jar, username: &str, password: &str, usecookie: i32) {
+    let client = reqwest::Client::builder()
+        .user_agent(fake_ua())
+        .build()
+        .unwrap();
+    let res = client
+        .post("https://www.wenku8.net/login.php")
+        .form(&[
+            ("username", username),
+            ("password", password),
+            ("usecookie", usecookie.to_string().as_str()),
+            ("action", "login"),
+        ])
+        .send()
+        .await;
+    cookie_jar.add_cookie_str(
+        res.unwrap()
+            .headers()
+            .get("Set-Cookie")
+            .unwrap()
+            .to_str()
+            .unwrap(),
+        &"https://www.wenku8.net/login.php"
+            .parse::<reqwest::Url>()
+            .unwrap(),
+    )
+}
