a669f616ac5a181e403f7ba737cf43bc2e09b2a1
yixinBC <yixinbc@foxmail.com>
2023-03-16 22:20:13 +0800

    chore:be:api:struct

--- src-tauri/src/api/wenku8.rs
+++ src-tauri/src/api/wenku8.rs
@@ -19,8 +19,55 @@ async fn login(
     res
 }
 
+pub struct Author<'a> {
+    name: String,
+    client: &'a reqwest::Client,
+}
+
+pub struct Book<'a> {
+    id: String,
+    url: String,
+    name: String,
+    client: &'a reqwest::Client,
+    cover_url: String,
+    author: Author<'a>,
+    meta_info: scraper::Html,
+}
+
 pub struct Wenku8 {
-    _client: reqwest::Client,
+    client: reqwest::Client,
+}
+
+impl<'a> Author<'a> {
+    pub fn new(client: &'a reqwest::Client, name: String) -> Self {
+        Self { name, client }
+    }
+}
+
+impl<'a> Book<'a> {
+    pub async fn new(client: &'a reqwest::Client, id: String) -> Book<'a> {
+        let url = format!("https://www.wenku8.net/book/{}.htm", id);
+        let meta_info = scraper::Html::parse_fragment(
+            client
+                .get(&url)
+                .send()
+                .await
+                .unwrap()
+                .text()
+                .await
+                .unwrap()
+                .as_str(),
+        );
+        Self {
+            id,
+            url,
+            meta_info,
+            name: String::new(),
+            client: client,
+            cover_url: String::new(),
+            author: Author::new(client, String::new()),
+        }
+    }
 }
 
 impl Wenku8 {
@@ -33,6 +80,6 @@ impl Wenku8 {
         let _ = login(&client, &name, &password, 315360000)
             .await
             .expect("login failed");
-        Self { _client: client }
+        Self { client }
     }
 }
